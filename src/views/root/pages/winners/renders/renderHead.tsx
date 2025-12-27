7ee6a9c0c9c00900055b81fd49ef858f8527d770
F_edoS <m-f-a1999@yandex.ru>
2025-12-27 11:58:44 +0700

    Upd

--- src/views/root/pages/winners/renders/renderHead.tsx
+++ src/views/root/pages/winners/renders/renderHead.tsx
@@ -9,11 +9,20 @@ const renderHead: I['renderHead'] = function () {
         <div className="page__head _COL _COL_H_CENTER">
             <h3 className="page__title">Победители розыгрышей</h3>
             <p className="page__description">
-                Подробную информацию <br className="_MOBILE" />смотри в{' '}
+                Подробную информацию <br className="_MOBILE" />
+                смотри в{' '}
                 <Link className="page__descriptionLink" pageName="auth-login">
                     Личном кабинете
                 </Link>
             </p>
+            <a
+                className="page__headLink"
+                href={`/upload/docs/cng_prizers.pdf?${new Date().getTime()}`}
+                target="_blank"
+                rel="noreferrer"
+            >
+                <span>Обменные призы</span>
+            </a>
         </div>
     );
 };
